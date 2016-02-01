cd5ff954ef9657f278a6c704f583710b96a34b0c
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-02-01 22:57:25 +0530

    returning in [[],[]] format

--- social_handles_data/facebook_data.py
+++ social_handles_data/facebook_data.py
@@ -5,6 +5,7 @@ Date: 28 Jan, 2016
 Fetch video information from facebook GraphAPI
 """
 
+from __future__ import division
 import facebook   # python client lib for facebook GraphAPI 
 import json
 
@@ -14,8 +15,8 @@ def _generate_x_values():
     Generates array of values from 0-40.
     """
     x_values = []
-    for val in xrange(0, 40, 1):
-        x_values.append(val)
+    for val in xrange(0, 41, 1):
+        x_values.append(val*100/41)
     return x_values
 
 
@@ -23,23 +24,19 @@ def fetch_data():
     """
     Fetch facebook retention values for video that we upload!
     :param video_id: facebook video id for glamrs video.
-
-    Return Values: Json object containing  [x_values, y_values].  
-    :x_values: X Axis values for Line Graph. This would be constant. 
-    :y_values: Y Axis values for Line Graph.
     """
-    print "In fetch data facebook--------------------------------\n\n"
-    print facebook
-    graph = facebook.GraphAPI(access_token='CAAGYi3qeYAIBAFmoT0wGbny9QwV5ZBHPnxdwtLV68a58hOBqNllStsfw1CV7EkP5Nh8vvfarDqqWZC0s9HZCxuYCnXiz6sSKVPsgyZAzFsHEYw5NtDY5JAAmqIZB8BZCRcJkSz1IkLl54lGkMOz8RlMDU7nkrcbBT4dueZASeCe6gwxYm5xGdeVX9Mm34xuzyUnHBsZCVN1AdgZDZD', timeout=100)
+    graph = facebook.GraphAPI(access_token='CAAGYi3qeYAIBAOaska4bnaApnDoRcZBd0XxV9WkDSzfVE8z9MTQKGR4BNZCJF3vc84SpSmhaeIwTXcVnIvGnWqu72reJHSRAvJmAZCHLpIyZCKssXCP5fgFLPxolgPGAZCZCBnGoyOEquPdjgpkEPU3vKBIvR1ODPd2yi3nx3TKI31WZBfrxrjRBhmC3aBJrQmbvXMMntlwuQZDZD', timeout=100)
     # access token have to be refreshed after specifies timeout parameter. Get it from https://developers.facebook.com/tools/explorer/
     y_values = graph.request("319863888091100_949859058424910/insights/post_video_retention_graph/lifetime")['data'][0]['values'][0]['value']
-    '''
-    final = []
-    for val in xrange(0, 40, 2):
-        final.append(y_values[val])
-    '''
-    x_values = _generate_x_values()
-    return json.dumps({"x_values": x_values, "y_values":y_values})
+    output = []
+    count = 0
+    for y_value in y_values:
+        output.append([count * 2.5, y_value])
+        count = count + 1
+
+    # return json.dumps({"x_values": x_values, "y_values":y_values})
+    return json.dumps(output)
+
 
 if __name__ == "__main__":
    print fetch_data()

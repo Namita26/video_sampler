035a047d6e2cb2f1b2599e663d6fd293d9eca363
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-02-01 22:56:49 +0530

    added two new classes for fb and youtube videos

--- apis/models.py
+++ apis/models.py
@@ -4,12 +4,37 @@ from flask import jsonify
 import flask.ext.sqlalchemy
 import flask.ext.restless
 from apis.connection import db
+from sqlalchemy.dialects.mysql import DOUBLE
 
+
+def _get_date():
+    return datetime.datetime.now()
+
+"""
 class Categories(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Unicode, unique=True)
 
     def __init__(self, name):
         self.name = name
+"""
+
+class Videos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prodCost = db.Column(DOUBLE)
+    producedOn = db.Column(db.Date, default=_get_date)
+
+
+class FBVideos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    videoId = db.Column(db.Integer, ForeignKey("Videos.id"), nullable=False)
+    publishedOn = db.Column(db.Date, default=_get_date)
+
+
+class YTVideos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    videoId = db.Column(db.Integer, ForeignKey("Videos.id"), nullable=False)
+    publishedOn = db.Column(db.Date, default=_get_date)
+
 
 db.create_all()

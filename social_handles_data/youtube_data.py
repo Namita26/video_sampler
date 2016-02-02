34aaf9631d76b3f3ce7438bde2d512e3df8b364a
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-02-02 20:33:06 +0530

    youtube api accepting id

--- social_handles_data/youtube_data.py
+++ social_handles_data/youtube_data.py
@@ -2,17 +2,17 @@ import requests
 import json
 
 
-def fetch_data():
+def fetch_data(id):
     """
     Fetch youtube retention values for video that we upload!
     :param video_id: facebook video id for glamrs video.
     """
-    res = requests.get('https://www.googleapis.com/youtube/analytics/v1/reports?ids=channel%3D%3DUCXyq6UjvT4dWjMOOiKuBncA&start-date=2016-01-01&end-date=2016-01-23&metrics=audienceWatchRatio&dimensions=elapsedVideoTimeRatio&filters=video%3D%3DUYpoq0-Lamw&key={AIzaSyDT2HJjNdzVRVbxZKWh4PN_AuCxWeqVPsE}', headers={'Authorization': 'Bearer ya29.ewLFB0uPhLSOEjb2SSZF0mSYf-8qu9pcext8sAkWT3WP9kBG92Eqa54k7S6GOAzHEiEU'})
+    res = requests.get('https://www.googleapis.com/youtube/analytics/v1/reports?ids=channel%3D%3DUCXyq6UjvT4dWjMOOiKuBncA&start-date=2016-01-01&end-date=2016-01-23&metrics=audienceWatchRatio&dimensions=elapsedVideoTimeRatio&filters=video%3D%3D' + id + '&key={AIzaSyDT2HJjNdzVRVbxZKWh4PN_AuCxWeqVPsE}', headers={'Authorization': 'Bearer ya29.fAJ4MiDT3fgqPDvgeVPlw763E5od5mL53wEgzzfhi2bz97Xy4WhkJbwmK69x0qJlNZLy'})
     youtubedata = res.json()
     output = []
-    for arr in youtubedata['rows']:
-        output.append([arr[0] * 100, arr[1]])
+    for arr in youtubedata["rows"]:
+        output.append(arr[1])
     return json.dumps(output)
 
 if __name__ == "__main__":
-    fetch_data()
+    print fetch_data('UYpoq0-Lamw')

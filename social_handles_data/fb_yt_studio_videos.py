3c74da4600eb5dca224243b8b647c19e94af6374
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-02-25 16:10:29 +0530

    inint file

--- social_handles_data/fb_yt_studio_videos.py
+++ social_handles_data/fb_yt_studio_videos.py
@@ -4,7 +4,9 @@ import csv
 import facebook
 import datetime
 
-google_token = "Bearer ya29.jQLtsYOGyFDwdL3x0ZrzVvVALnWgAhPKFtCiko5o8S8S5LoITJhNzXPPhrhfasqWNOyd"
+
+google_token = "Bearer ya29.kAK_5IQK061-wsBhUCYnbQ7JWXGpTXybCKlMMijlje2GoHRwwPEd4XL1-DBB9M9Ctjel"
+
 
 video_ids = [
     {
@@ -61,14 +63,12 @@ def get_youtube_video_ids():
     youtube_video_ids = []
     for each_dict in video_ids:
         youtube_video_ids.append(each_dict['yt'])
-    print "yt video ids n: " + str(len(youtube_video_ids))
     return youtube_video_ids
 
 def get_facebook_video_ids():
     facebook_video_ids = []
     for each_dict in video_ids:
         facebook_video_ids.append(each_dict['fb'])
-    print "fb video ids n: " + str(len(facebook_video_ids))
     return facebook_video_ids
 
 def get_youtube_title(title_info, video_id):
@@ -77,6 +77,11 @@ def get_youtube_title(title_info, video_id):
             title = i['title']
     return title
 
+
+def post_insights_object(video_id):
+    graph = facebook.GraphAPI(access_token='CAAGYi3qeYAIBAJTGZC5QdgWYaKQ71JvOICcYfKIeAXn8vKA1ZA2S2WnXoOZCC49llalYnMrwELeOTKF1UIgoDXBa2OjXz9PutEfLqN5g8wqxUBppvfkwWtluJkE2FQYYbbZBq1zW8598QmxYvJEv8Dqt6ZBviO3ZBX0jb6L8Fq9kzIIRTYHNZBvb2jCdXroBDMZD')
+    insights = graph.request("319863888091100_" + str(video_id) + "/insights/")
+    return json.dumps(insights)
 
 
 def fetch_data():
@@ -95,6 +100,7 @@ def fetch_data():
     youtube_video_ids = get_youtube_video_ids()
     facebook_video_ids = get_facebook_video_ids()
     final = []
+
 
     res = requests.get('https://www.googleapis.com/youtube/analytics/v1/reports?ids=channel%3D%3DUCXyq6UjvT4dWjMOOiKuBncA&start-date=' 
     + start_date +'&end-date=' + end_date + '&metrics=views%2Clikes%2Ccomments&filters=video%3D%3D' + ",".join(youtube_video_ids) + '&key={AIzaSyDT2HJjNdzVRVbxZKWh4PN_AuCxWeqVPsE}&dimensions=video', headers={'Authorization': google_token})
@@ -128,11 +134,20 @@ def fetch_data():
     sum_total_views = []
     sum_total_likes = []
     sum_total_comments = []
-
     i = 0
+
+    all_yb = []
+    for video_id in video_ids:
+        for yt_data in youtubedata['rows']:
+            if video_id['yt'] == yt_data[0]:
+                all_yb.append(yt_data)
+
+    youtubedata["rows"] = all_yb
+
     while i < len(video_ids):
         each_video = {}
         each_video['facebook'] = {'video_id': all_fb[i][0], 'views':all_fb[i][1], 'likes':all_fb[i][2], 'comments': all_fb[i][3], 'shares': all_fb[i][4], 'video_title': all_fb[i][5]}
+
         each_video['youtube'] = {'video_id': youtubedata['rows'][i][0], 'views': youtubedata['rows'][i][1], 'likes': youtubedata['rows'][i][2], 'comments': youtubedata['rows'][i][3], 'video_title': get_youtube_title(title_info, youtubedata['rows'][i][0])}
         each_video['fb_yt'] = {'total_views': all_fb[i][1] + youtubedata['rows'][i][1], 'total_likes': all_fb[i][2] + youtubedata['rows'][i][2], 'total_comments': all_fb[i][3] + youtubedata['rows'][i][3]}
         final.append(each_video)
@@ -151,12 +166,6 @@ def fetch_data():
         sum_total_comments.append(all_fb[i][3] + youtubedata['rows'][i][3])
 
         i = i + 1
-    print sum(facebook_grand_likes)
-    print sum(youtube_grand_likes)
-    print sum(facebook_grand_views)
-    print sum(youtube_grand_views)
-    print sum(facebook_grand_comments)
-    print sum(youtube_grand_comments)
     final.append(sum(facebook_grand_views))
     final.append(sum(youtube_grand_views))
     final.append(sum(sum_total_views))
@@ -170,7 +179,7 @@ def fetch_data():
     return json.dumps(final)
 
 
-def ifetch_data():
+def fetch_data_to_csv():
     """
     Fetch views, likes, comments and shares for YouTube and Facebook videos
     :Input video ids should be given in following format.

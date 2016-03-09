ef7326da21f36a386732bc768a66f7cd36a6eaab
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-03-09 15:29:24 +0530

    new craftsvilla links

--- crons/fetch_insights.sh
+++ crons/fetch_insights.sh
@@ -1,5 +1,26 @@
-curl http://localhost:5000/youtube_insights/\?ids\=kxB-fIFTcG0,kl_KhEJslN4,dv-LHg9X4-0,77VY8q2m0gk,sTlPQLXbX0g,yOcJUJgEsIg,ZMkDQGGPjDo,mn8yeT6Y-tQ,5qwgEoy4GoQ,LS-KIUdobXc,lxrVZInm0vI,pFrUnz-NO7A
+curl http://localhost:5000/facebook_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B898908596853290%5D%7D%2C+%7B%22brandname%22%3A+%22loreal%22%7D%5D'
+curl http://localhost:5000/youtube_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B%22pEn0H-R-YnU%22%5D%7D%2C+%7B%22brandname%22%3A+%22loreal%22%7D%5D'
+curl http://localhost:5000/fb_yt/\?ids\='video_ids=%5B%7B%22fb%22%3A+898908596853290%2C+%22yt%22%3A+%22pEn0H-R-YnU%22%7D%2C+%7B%22brand%22%3A+%22loreal%22%7D%5D'
 
-curl http://localhost:5000/facebook_insights/\?ids\=863863537024463,864289866981830,863813073696176,843279482416202,829381850472632,786952974715520,755004151243736,899305756813574,896394667104683,896632997080850,772150292862455,932669140143902
 
-curl http://localhost:5000/fb_yt/\?ids\='video_ids=%5B%7B%22fb%22%3A+863863537024463%2C+%22yt%22%3A+%22kxB-fIFTcG0%22%7D%2C+%7B%22fb%22%3A+864289866981830%2C+%22yt%22%3A+%22kl_KhEJslN4%22%7D%2C+%7B%22fb%22%3A+863813073696176%2C+%22yt%22%3A+%22dv-LHg9X4-0%22%7D%2C+%7B%22fb%22%3A+843279482416202%2C+%22yt%22%3A+%2277VY8q2m0gk%22%7D%2C+%7B%22fb%22%3A+829381850472632%2C+%22yt%22%3A+%22sTlPQLXbX0g%22%7D%2C+%7B%22fb%22%3A+786952974715520%2C+%22yt%22%3A+%22yOcJUJgEsIg%22%7D%2C+%7B%22fb%22%3A+755004151243736%2C+%22yt%22%3A+%22ZMkDQGGPjDo%22%7D%2C+%7B%22fb%22%3A+899305756813574%2C+%22yt%22%3A+%22mn8yeT6Y-tQ%22%7D%2C+%7B%22fb%22%3A+896394667104683%2C+%22yt%22%3A+%225qwgEoy4GoQ%22%7D%2C+%7B%22fb%22%3A+896632997080850%2C+%22yt%22%3A+%22LS-KIUdobXc%22%7D%2C+%7B%22fb%22%3A+772150292862455%2C+%22yt%22%3A+%22lxrVZInm0vI%22%7D%2C+%7B%22fb%22%3A+932669140143902%2C+%22yt%22%3A+%22pFrUnz-NO7A%22%7D%5D'
+curl http://localhost:5000/facebook_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B929871233757026%2C+947482865329196%5D%7D%2C+%7B%22brandname%22%3A+%22godrej%22%7D%5D'
+curl http://localhost:5000/youtube_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B%22J5xNIJlfBAw%22%2C+%22NlDWRmDCa_o%22%5D%7D%2C+%7B%22brandname%22%3A+%22godrej%22%7D%5D'
+curl http://localhost:5000/fb_yt/\?ids\='video_ids=%5B%7B%22fb%22%3A+929871233757026%2C+%22yt%22%3A+%22J5xNIJlfBAw%22%7D%2C+%7B%22fb%22%3A+947482865329196%2C+%22yt%22%3A+%22NlDWRmDCa_o%22%7D%2C+%7B%22brand%22%3A+%22godrej%22%7D%5D'
+
+
+curl http://localhost:5000/facebook_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B851685721575578%2C+871392976271519%5D%7D%2C+%7B%22brandname%22%3A+%22maybelline%22%7D%5D'
+curl http://localhost:5000/youtube_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B%22ubePCsGXcUE%22%2C+%22-7MV-cspVXM%22%5D%7D%2C+%7B%22brandname%22%3A+%22maybelline%22%7D%5D'
+curl http://localhost:5000/fb_yt/\?ids\='video_ids=%5B%7B%22fb%22%3A+851685721575578%2C+%22yt%22%3A+%22ubePCsGXcUE%22%7D%2C+%7B%22fb%22%3A+871392976271519%2C+%22yt%22%3A+%22-7MV-cspVXM%22%7D%2C+%7B%22brand%22%3A+%22maybelline%22%7D%5D'
+
+curl http://localhost:5000/facebook_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B863863537024463%2C+864289866981830%2C+863813073696176%2C+843279482416202%2C+829381850472632%2C+786952974715520%2C+755004151243736%2C+899305756813574%2C+896394667104683%2C+896632997080850%2C+772150292862455%2C+932669140143902%5D%7D%2C+%7B%22brandname%22%3A+%22studiowest%22%7D%5D'
+
+curl http://localhost:5000/youtube_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B%22kxB-fIFTcG0%22%2C+%22kl_KhEJslN4%22%2C+%22dv-LHg9X4-0%22%2C+%2277VY8q2m0gk%22%2C+%22sTlPQLXbX0g%22%2C+%22yOcJUJgEsIg%22%2C+%22ZMkDQGGPjDo%22%2C+%22mn8yeT6Y-tQ%22%2C+%225qwgEoy4GoQ%22%2C+%22LS-KIUdobXc%22%2C+%22lxrVZInm0vI%22%2C+%22pFrUnz-NO7A%22%5D%7D%2C+%7B%22brandname%22%3A+%22studiowest%22%7D%5D'
+
+curl http://localhost:5000/fb_yt/\?ids\='video_ids=%5B%7B%22fb%22%3A+863863537024463%2C+%22yt%22%3A+%22kxB-fIFTcG0%22%7D%2C+%7B%22fb%22%3A+864289866981830%2C+%22yt%22%3A+%22kl_KhEJslN4%22%7D%2C+%7B%22fb%22%3A+863813073696176%2C+%22yt%22%3A+%22dv-LHg9X4-0%22%7D%2C+%7B%22fb%22%3A+843279482416202%2C+%22yt%22%3A+%2277VY8q2m0gk%22%7D%2C+%7B%22fb%22%3A+829381850472632%2C+%22yt%22%3A+%22sTlPQLXbX0g%22%7D%2C+%7B%22fb%22%3A+786952974715520%2C+%22yt%22%3A+%22yOcJUJgEsIg%22%7D%2C+%7B%22fb%22%3A+755004151243736%2C+%22yt%22%3A+%22ZMkDQGGPjDo%22%7D%2C+%7B%22fb%22%3A+899305756813574%2C+%22yt%22%3A+%22mn8yeT6Y-tQ%22%7D%2C+%7B%22fb%22%3A+896394667104683%2C+%22yt%22%3A+%225qwgEoy4GoQ%22%7D%2C+%7B%22fb%22%3A+896632997080850%2C+%22yt%22%3A+%22LS-KIUdobXc%22%7D%2C+%7B%22fb%22%3A+772150292862455%2C+%22yt%22%3A+%22lxrVZInm0vI%22%7D%2C+%7B%22fb%22%3A+932669140143902%2C+%22yt%22%3A+%22pFrUnz-NO7A%22%7D%2C+%7B%22brand%22%3A+%22studiowest%22%7D%5D'
+
+
+curl http://localhost:5000/youtube_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B%22DRoNTDRJNuU%22%5D%7D%2C+%7B%22brandname%22%3A+%22craftsvilla%22%7D%5D'
+
+curl http://localhost:5000/facebook_insights/\?ids\='video_ids=%5B%7B%22video_ids%22%3A+%5B925173684226781%5D%7D%2C+%7B%22brandname%22%3A+%22craftsvilla%22%7D%5D'
+
+curl http://localhost:5000/fb_yt/\?ids\='video_ids=%5B%7B%22fb%22%3A+925173684226781%2C+%22yt%22%3A+%22DRoNTDRJNuU%22%7D%2C+%7B%22brand%22%3A+%22craftsvilla%22%7D%5D'

4d487ec6f07c0e0dbdc34b3e4fed6ed23d55bfd5
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-03-15 14:36:09 +0530

    end date is not global now

--- social_handles_data/utils/fb.py
+++ social_handles_data/utils/fb.py
@@ -8,9 +8,9 @@ from social_handles_data.utils.file_util import FileUtil
 def facebook_graph_object():
     return facebook.GraphAPI(access_token='CAAGYi3qeYAIBAOsFnZCg7SZACH9cdXYxz3fOA3wt3y3RFKeRl8bZBZCRL5ZBs7lLBYPbknrtQdm13RmmchoaYl7sYHV0PyPlxUaVl0FTWhNIOpd5uiJbTJ0pCuXSR6GDIPbWEVuEacFNk0MexAAaDH58b4mcCbEaIE4XNIf221HVlRGYvhuds')
 
-end_date = datetime.datetime.today().strftime('%Y-%m-%d')
 
 def post_insights_object(video_id, brand_name):
+    end_date = datetime.datetime.today().strftime('%Y-%m-%d')
     graph = facebook_graph_object()
     insights = graph.request("319863888091100_" + str(video_id) + "/insights/")
     filepath = 'social_handles_data/' + brand_name + '/' + end_date +'_fb_insights.json'
@@ -24,6 +24,7 @@ def post_insights_object(video_id, brand_name):
     return insights
 
 def post_meta_info(video_id):
+    end_date = datetime.datetime.today().strftime('%Y-%m-%d')
     meta = {}
     print video_id, ""
     graph = facebook_graph_object()
@@ -33,6 +34,7 @@ def post_meta_info(video_id):
     return meta
 
 def get_insights(video_ids, brand_name):
+    end_date = datetime.datetime.today().strftime('%Y-%m-%d')
     # video_ids = video_ids.split(',')
     for video_id in video_ids:
         post_insights_object(video_id, brand_name)

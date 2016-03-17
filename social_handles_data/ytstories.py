28f85675ef089f913bcd40336b60e0ac8b820aeb
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-03-17 07:11:38 +0530

    yt functions

--- social_handles_data/ytstories.py
+++ social_handles_data/ytstories.py
@@ -11,6 +11,19 @@ def get_youtube_title(title_info, video_id):
     return title
 
 
+def get_youtube_created_time(title_info, video_id):
+    for i in title_info:
+        if i['id'] == video_id:
+            ctime = i['created_time']
+    return ctime
+
+
+def get_youtube_uniques(uniques, video_id):
+    for i in uniques:
+        if i['id'] == video_id:
+            uniques = i['uniques']
+    return uniques
+
 def get_youtube_stories(video_ids, start_date, end_date, brand_name):
     youtube_video_ids = get_youtube_video_ids(video_ids)
     insights_file = FileUtil.readJson('social_handles_data/' +brand_name+ '/'+ end_date +'yt_stats.json')
@@ -26,15 +39,18 @@ def get_youtube_stories(video_ids, start_date, end_date, brand_name):
         return post_insights_object_combined(",".join(youtube_video_ids), brand_name)
 
 
-def get_grand_youtube_stories(all_yb):
+def get_grand_youtube_stories(all_yb, uniques):
     # all_yb = get_youtube_stories()[0]
     grand = {
         "youtube_grand_likes": 0,
         "youtube_grand_views": 0,
-        "youtube_grand_comments": 0
+        "youtube_grand_comments": 0,
+        "youtube_grand_uniques": 0
     }
     for y_video in all_yb:
         grand['youtube_grand_likes'] += y_video[2]
         grand['youtube_grand_views'] += y_video[1]
         grand['youtube_grand_comments'] += y_video[3]
+        grand['youtube_grand_uniques'] = sum([unique['uniques'] for unique in uniques])
     return grand
+

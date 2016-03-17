1c1b86d14ea3592d11a5e4cf853d410ba910ee77
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-03-17 08:47:31 +0530

    glamrs website values fetch

--- social_handles_data/utils/fetch_yt_analytics.py
+++ social_handles_data/utils/fetch_yt_analytics.py
@@ -2,15 +2,25 @@ import requests
 
 
 def refresh_access_token():
-    refresh_token = "1/eP7zTuVNkjTELesHQknC5Rb8J5O4AVtU_XfWrZXDWhEMEudVrK5jSpoR30zcRFq6"
+
     payload = {
-        'client_id':  "816284744569-hblbg79baprfv7472kopc7q0ql53bn9b.apps.googleusercontent.com",
-        'client_secret': 'J75434FzIp5RY87K-IVYkGaF',
-        'refresh_token': refresh_token,
-        'grant_type': 'refresh_token'
+        "refresh_token": "1/eP7zTuVNkjTELesHQknC5Rb8J5O4AVtU_XfWrZXDWhEMEudVrK5jSpoR30zcRFq6",
+        "client_secret": "J75434FzIp5RY87K-IVYkGaF",
+        "client_id": "816284744569-hblbg79baprfv7472kopc7q0ql53bn9b.apps.googleusercontent.com",
+        "grant_type": "refresh_token"
     }
     refreshed_access_token = requests.post("https://accounts.google.com/o/oauth2/token", data=payload).json()
     return refreshed_access_token
+
+
+def glamrs_youtube_views(video_id):
+    vmap = {
+        "ubePCsGXcUE": "7-genius-ways-to-use-a-compact-powder",
+        "-7MV-cspVXM": "lipstick-hacks-every-girl-needs-to-know"
+    } 
+    video = vmap[video_id]
+    res = requests.get("https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A67237418&start-date=7daysAgo&end-date=today&metrics=ga%3Apageviews&dimensions=ga%3ApagePath&filters=ga%3ApagePath%3D%40" + video, headers={"Authorization": "Bearer ya29.qAKeZqB9J-8xCd_GpAv5-g5_WtgRoFeF497oquT6kmvWA4akGRmh0mfOipOcVT9xNA"})
+    return int(res.json()['rows'][0][1])
 
 if __name__ == "__main__":
     print refresh_access_token()

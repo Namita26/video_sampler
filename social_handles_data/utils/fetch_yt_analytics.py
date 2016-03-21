d117550ac8ed063d55758c96c0a6b5543307e592
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-03-21 11:33:27 +0530

    token

--- social_handles_data/utils/fetch_yt_analytics.py
+++ social_handles_data/utils/fetch_yt_analytics.py
@@ -17,9 +17,9 @@ def glamrs_youtube_views(video_id):
     vmap = {
         "ubePCsGXcUE": "7-genius-ways-to-use-a-compact-powder",
         "-7MV-cspVXM": "lipstick-hacks-every-girl-needs-to-know"
-    } 
+    }
     video = vmap[video_id]
-    res = requests.get("https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A67237418&start-date=2015-01-01&end-date=today&metrics=ga%3Apageviews&dimensions=ga%3ApagePath&filters=ga%3ApagePath%3D%40" + video, headers={"Authorization": "Bearer ya29.qQKfSQc-onYtA1li3lqhkC3ZzHCMqAdI6k2BCu8Zz09Lp0wKdN5fBXACkMy2Un8uvQ"})
+    res = requests.get("https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A67237418&start-date=2015-01-01&end-date=today&metrics=ga%3Apageviews&dimensions=ga%3ApagePath&filters=ga%3ApagePath%3D%40" + video, headers={"Authorization": "Bearer ya29.rAJ2PuuKSLB6W1YS-1oIoye8maCf01pAADElFem-NJZnym4hdUCXSfF3HBVdYsQG_g"})
     return sum([int(row[1]) for row in res.json()['rows']])
 
 if __name__ == "__main__":

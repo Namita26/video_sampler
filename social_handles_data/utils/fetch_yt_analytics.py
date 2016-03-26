45fc5a7d838ff186b52ea591f6a89c548900dacf
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-03-26 08:33:13 +0530

    token

--- social_handles_data/utils/fetch_yt_analytics.py
+++ social_handles_data/utils/fetch_yt_analytics.py
@@ -19,7 +19,7 @@ def glamrs_youtube_views(video_id):
         "-7MV-cspVXM": "lipstick-hacks-every-girl-needs-to-know"
     }
     video = vmap[video_id]
-    res = requests.get("https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A67237418&start-date=2015-01-01&end-date=today&metrics=ga%3Apageviews&dimensions=ga%3ApagePath&filters=ga%3ApagePath%3D%40" + video, headers={"Authorization": "Bearer ya29.rgLNcBciM3vi56WkuaKfNmYyi-GlIFXUlYsec2ENUAVr7RGqdxWRw24-IAEiMIQZ4g"})
+    res = requests.get("https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A67237418&start-date=2015-01-01&end-date=today&metrics=ga%3Apageviews&dimensions=ga%3ApagePath&filters=ga%3ApagePath%3D%40" + video, headers={"Authorization": "Bearer ya29.sQKace0z6EIAVw5sHLJpvRWBh7YDcV9cOUXRUNjfc9VKq_mUUSuROMO6F4jlYpO0gw"})
     return sum([int(row[1]) for row in res.json()['rows']])
 
 if __name__ == "__main__":
